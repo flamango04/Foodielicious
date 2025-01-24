3c610e93a5155f02dcbaae6cdaa40b7896a74fef
Zihe Wang <103771205+ZiheHerzWang@users.noreply.github.com>
2025-01-23 23:47:20 -0500

    testing elasticsearch
    successfully connected to elasticsearch and upload a json file with proper format. Also testing searching and it works.

---
+++ real_process/upload_bulk.py
@@ -0,0 +1,32 @@
+from elasticsearch import Elasticsearch, helpers
+import json
+
+# Elasticsearch connection configuration
+es = Elasticsearch(
+    ["https://localhost:9200"], 
+    basic_auth=("elastic", "F-L*fKaaY+mOC2SY=DVE"), 
+    verify_certs=True,
+    ca_certs="/Users/macbook/Documents/GitHub/Foodielicious/http_ca.crt",  
+)
+
+def bulk_upload_to_es(es, bulk_file_path, index_name):
+    def generate_actions():
+        with open(bulk_file_path, mode="r", encoding="utf-8") as f:
+            lines = f.readlines()
+            for i in range(0, len(lines), 2):
+                index_line = json.loads(lines[i].strip())
+                doc_line = json.loads(lines[i + 1].strip())
+                yield {
+                    "_index": index_name,
+                    "_id": index_line["index"]["_id"],
+                    "_source": doc_line,
+                }
+
+    helpers.bulk(es, generate_actions())
+
+bulk_file_path = "../dataset/elastic_bulk_recipes.json"
+index_name = "ingredient"
+
+bulk_upload_to_es(es, bulk_file_path, index_name)
+
+print(f"Bulk data successfully uploaded to index {index_name}")
